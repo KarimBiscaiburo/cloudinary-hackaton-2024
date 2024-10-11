48cd3ad5313362fb8791bda821c894de50301968
Karim Biscaiburo <kbiscaiburo@gmail.com>
2024-10-11 19:18:41 -0300

    Iniciando repositorio

---
+++ src/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Creepster, Special_Elite } from "next/font/google"
+
+export const creepster = Creepster({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap"
+  });
+  
+export const special_elite = Special_Elite({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap"
+  });
